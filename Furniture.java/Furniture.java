64179bc781ae3648e5aaa13b9ca8792be1a8ee2c
Jessie0611~ <imjustme0611@hotmail.com>
2024-08-27 19:21:24 -0400

    Week two content

---
+++ Furniture.java/Furniture.java
@@ -0,0 +1,23 @@
+public class Furniture{
+	public static void main(String args[]){
+		String itemName = "TV Stand";
+		double retailPrice = 325.00;
+		double wholesalePrice = 200.00;
+		double salePrice;
+		double profit;
+		double saleProfit;
+        
+        profit = retailPrice - wholesalePrice;
+        salePrice = retailPrice * 0.75;
+        saleProfit = salePrice - wholesalePrice;
+
+		System.out.println("Item Name: " + itemName);
+		System.out.println("Retail Price: $" + retailPrice);
+		System.out.println("Wholesale Price: $" + wholesalePrice);
+		System.out.println("Profit: $" + profit);	
+		System.out.println("Sale Price: $" + salePrice);
+		System.out.println("Sale Profit: $" + saleProfit);
+		System.exit(0);
+		System.exit(0);
+	}
+}
