dbac758f2609b3a43a5ef1b53907a5f17e2cdf49
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-02 17:20:48 -0400

    Finish assignment

---
+++ NewAge/NewAge.java
@@ -0,0 +1,14 @@
+public class NewAge{
+   public static void main(String args[]){
+    int currentAge = 33;
+    int currentYear = 2024;
+    int newAge = currentAge + (2050 - currentYear);
+		
+      newAge = currentAge + (2050 - currentYear);
+
+      System.out.println("My Current Age is " + currentAge);
+      System.out.println("I will be " + newAge + " in 2050.");
+
+      System.exit(0);
+   }
+}
