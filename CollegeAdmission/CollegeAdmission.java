a9d6d4c5ae15c1a91cea0d3cab8136d7a15eda74
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-03 17:09:41 -0400

    Add week 3 template

---
+++ CollegeAdmission/CollegeAdmission.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane; 
+public class CollegeAdmission{
+   public static void main(String args[]){ 
+     // Declare variables   
+     // Get input and convert to correct data type
+     // Test using admission requirements and print Accept or Reject 
+     if( testScore >= 90 )
+     {
+	if( classRank >= 25)
+        {
+     	   System.out.println("Accept");
+        }
+	else
+	   System.out.println("Reject"); 
+     }
+     else 
+     {
+	if( testScore >= 80 )
+	{
+	   if( classRank >= 50 )
+	      System.out.println("Accept");
+	   else
+	      System.out.println("Reject");
+	}
+	else
+	{
+           if( testScore >= 70 )
+	   {
+		if( classRank >=75 ) 
+		   System.out.println("Accept");
+		else
+		   System.out.println("Reject");
+	   }
+	   else
+		System.out.println("Reject");
+        }
+      }
+   }
+}
