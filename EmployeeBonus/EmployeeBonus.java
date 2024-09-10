fde568b74c12e34c0e6bbb5806db3da2bcb16fbb
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-10 19:07:56 -0400

    Complete lab 4.1,4.2

--- EmployeeBonus/EmployeeBonus.java
+++ EmployeeBonus/EmployeeBonus.java
@@ -1,10 +1,6 @@
-// EmployeeBonus.java - This program calculates an employee's productivity bonus.
 import javax.swing.*;
-public class EmployeeBonus
-{
-public static void main(String args[])
-{
-// Declare and initialize variables here.
+public class EmployeeBonus{
+public static void main(String args[]){
 String employeeName;
 double numTransactions;
 String transactString;
@@ -18,21 +14,25 @@ final double BONUS_1 = 50.00;
 final double BONUS_2 = 75.00;
 final double BONUS_3 = 100.00;
 final double BONUS_4 = 200.00;
-// This is the work done in the housekeeping() method
-employeeName = JOptionPane.showInputDialog("Enter employee's name:
-");
+
+employeeName = JOptionPane.showInputDialog("Enter employee's name: ");
 shiftString = JOptionPane.showInputDialog("Enter number of shifts: ");
-transactString = JOptionPane.showInputDialog("Enter number of
-transactions: ");
-dollarString = JOptionPane.showInputDialog("Enter transactions dollar
-value: ");
+transactString = JOptionPane.showInputDialog("Enter number of transactions: ");
+dollarString = JOptionPane.showInputDialog("Enter transactions dollar value: ");
 numShifts = Double.parseDouble(shiftString);
 numTransactions = Double.parseDouble(transactString);
 dollarValue = Double.parseDouble(dollarString);
-// This is the work done in the detailLoop() method
-// Write your code here
-// This is the work done in the endOfJob() method
-// Output.
+
+score = numTransactions * dollarValue;
+if (score <= 200) {
+    bonus = BONUS_4;
+} else if (score <= 70) {
+    bonus = BONUS_3;
+} else if (score <= 31) {
+    bonus = BONUS_2;
+} else {
+    bonus = BONUS_1;
+}
 System.out.println("Employee Name: " + employeeName);
 System.out.println("Employee Bonus: $" + bonus);
 System.exit(0);
--- LargeSmall/LargeSmall.java
+++ LargeSmall/LargeSmall.java
@@ -1,17 +1,30 @@
-// LargeSmall.java - This program calculates the largest and smallest of three
-integer values.
-public class LargeSmall
-{
-public static void main(String args[])
-{
-// This is the work done in the housekeeping() method
-// Declare and initialize variables here.
-int largest; // Largest of the three values.
-int smallest; // Smallest of the three values.
-// This is the work done in the detailLoop() method
-//Write assignment, if, or if else statements here as appropriate.
-// This is the work done in the endOfJob() method
-// Output largest and smallest number.
+public class LargeSmall{
+public static void main(String args[]){
+    int num1 = -50;
+    int num2 = 53;
+    int num3 = 78;
+    int largest;
+    int smallest;
+
+    largest = num1;
+    smallest = num1;
+    
+if (num2 > largest){
+    largest = num2;
+}
+if (num3 > largest){
+    largest = num3;
+}
+else largest = num1;
+
+if (num2 < smallest){
+    smallest = num2;
+}
+if (num3 < smallest){
+    smallest = num3;
+}
+else smallest = num1;
+
 System.out.println("The largest value is " + largest);
 System.out.println("The smallest value is " + smallest);
 }
