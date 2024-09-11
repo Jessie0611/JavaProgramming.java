c25fe5edfd92cdd22ae05f6709dee3e0d0baf61d
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-10 20:04:21 -0400

    Complete labs, will work on last lab tomorrow

--- EmployeeBonus/EmployeeBonus2.java
+++ EmployeeBonus/EmployeeBonus2.java
@@ -1,37 +1,40 @@
-// EmployeeBonus2.java - This program calculates an employee's yearly bonus.
 import javax.swing.*;
-public class EmployeeBonus2
-{
-public static void main(String args[])
-{
-// Declare and initialize variables.
-String employeeName;
-String salaryString;
-double employeeSalary;
-String ratingString;
-int employeeRating;
-double employeeBonus;
-final double BONUS_1 = .25;
-final double BONUS_2 = .15;
-final double BONUS_3 = .10;
-final double NO_BONUS = 0.00;
-final int RATING_1 = 1;
-final int RATING_2 = 2;
-final int RATING_3 = 3;
-// This is the work done in the housekeeping() method
-// Get user input.
+public class EmployeeBonus2{
+public static void main(String args[]){
+    String employeeName;
+    String salaryString;
+    double employeeSalary;
+    String ratingString;
+    int employeeRating;
+    double employeeBonus;
+    final double BONUS_1 = .25;
+    final double BONUS_2 = .15;
+    final double BONUS_3 = .10;
+    final double NO_BONUS = 0.00;
+    final int RATING_1 = 1;
+    final int RATING_2 = 2;
+    final int RATING_3 = 3;
+
 employeeName = JOptionPane.showInputDialog("Enter employee's name: ");
-salaryString = JOptionPane.showInputDialog("Enter employee's yearly
-salary: ");
-ratingString = JOptionPane.showInputDialog("Enter employee's
-performance rating: ");
-// Convert Strings to int or double.
+salaryString = JOptionPane.showInputDialog("Enter employee's yearly salary: ");
+ratingString = JOptionPane.showInputDialog("Enter employee's performance rating: ");
 employeeSalary = Double.parseDouble(salaryString);
 employeeRating = Integer.parseInt(ratingString);
-// This is the work done in the detailLoop() method
-// Use switch statement here to calculate bonus based on rating.
-// This is the work done in the endOfJob() method
-// Output.
+
+employeeBonus = NO_BONUS;
+switch (employeeRating){
+    case 1:
+        employeeBonus = employeeSalary * BONUS_1;
+        break;
+    case 2:
+        employeeBonus = employeeSalary * BONUS_2;
+        break;
+    case 3:
+        employeeBonus = employeeSalary * BONUS_3;
+        break;
+}
+
+
 System.out.println("Employee Name " + employeeName);
 System.out.println("Employee Salary $" + employeeSalary);
 System.out.println("Employee Rating " + employeeRating);
