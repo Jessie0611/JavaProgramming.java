b74f3d1d5375cbc2e942632b85c10106e50e154b
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-15 16:02:10 -0400

    Finish week 4 assignemtns

--- Airline/Airline.java
+++ Airline/Airline.java
@@ -1,5 +1,3 @@
-// Airline.java - This program determines if an airline passenger is
-// eligible for a 25% discount.
 import javax.swing.*;
 public class Airline{
 public static void main(String args[]){
@@ -10,11 +8,15 @@ int passengerAge = 0;
 passengerName = JOptionPane.showInputDialog("Enter passenger's name: ");
 ageString = JOptionPane.showInputDialog("Enter passenger's age: ");
 passengerAge = Integer.parseInt(ageString);
-// This is the work done in the detailLoop() method
 
-// Test to see if this customer is eligible for a 25% discount.
+boolean eligibleForDiscount = (passengerAge <= 6) || (passengerAge >= 65);
 
-// This is the work done in the endOfJob() method
+if (eligibleForDiscount) {
+    System.out.println(passengerName + " is eligible for a 25% discount.");
+} else {
+    System.out.println(passengerName + " is not eligible for a 25% discount.");
+}
+
 System.exit(0);
 }
 }
