6ec1eb0a1fd6e1640984086dc760d7c364e16eae
Jessie0611~ <imjustme0611@hotmail.com>
2024-09-17 20:10:03 -0400

    Add week 5

--- MovieGuide/MovieGuide.java
+++ MovieGuide/MovieGuide.java
@@ -8,9 +8,14 @@ int numPatrons = 0;
 // This is the work done in the detailLoop() method
 // Convert to double.
 // Write while loop here
+numStars = JOptionPane.showInputDialog("Enter star rating:");
+
 while(numStars >= 0; numStars <= 5){
-    
+    totalStars += numStars;
+    numPatrons++;
 }
+
+averageStars = totalStars / numPatrons;
 
 System.out.println("Average Star Value: " + averageStars);
 System.exit(0);
