b3bb77f7f2ef95066977f15db217747227b92231
Jessie0611~ <imjustme0611@hotmail.com>
2024-10-05 14:38:06 -0400

    Complete lab 2

--- MichiganCities/MichiganCities.java
+++ MichiganCities/MichiganCities.java
@@ -1,20 +1,34 @@
 import javax.swing.*;
+
 public class MichiganCities{
-public static void main(String args[]) throws Exception{
-// Declare variables.
-    String inCity; // name of city to look up in array.
-// Initialized array of cities in Michigan.
-    String citiesInMichigan[] = {"Acme", "Albion", "Detroit", "Watervliet", "Coloma", "Saginaw", "Richland", "Glenn", "Midland", "Brooklyn"};
-    boolean foundIt = false; // Flag variable.
-    int x; // Loop control variable.
-// Get user input.
-    inCity = JOptionPane.showInputDialog("Enter name of city: ");
-// Write your loop here.
-// Write your test statement here to see if there is
-// a match. Set the flag to true if city is found.
-    
-// Test to see if city was not found to determine if
-// "Not a city in Michigan" message should be printed.
-    System.exit(0);
+    public static void main(String args[]) throws Exception{
+        String inCity;
+        String citiesInMichigan[] = {"Acme", "Albion", "Detroit", "Watervliet", "Coloma", "Saginaw", "Richland", "Glenn", "Midland", "Brooklyn"};
+        String FINISH = "X";
+
+        while (true) {
+            inCity = JOptionPane.showInputDialog("Enter name of city: ");
+            
+            if (inCity.equalsIgnoreCase(FINISH)) {
+                break; 
+            }
+
+            boolean foundIt = false;
+
+            for (String city : citiesInMichigan) {
+                if (city.equalsIgnoreCase(inCity)) {
+                    foundIt = true;
+                    break;
+                }
+            }
+
+            if (foundIt) {
+                JOptionPane.showMessageDialog(null, inCity + " is a city in Michigan.");
+            } else {
+                JOptionPane.showMessageDialog(null, inCity + " is not a city in Michigan.");
+            }
+        }
+
+        System.exit(0);
     }
 }
